fa3d29655116694aca129cb6c7a477b3720a8057
Pat <padr3ko@gmail.com>
2018-05-06 11:47:48 -0400

    Adding html, css, img, font folders and file for project

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Author: Patrick Furbert
+//Date: 
+
+$(document).ready(function(){
+
+//insert jQuery stuff here...
+
+$(".hamburger").click(function(){
+	$(".overlay").css("display", "block");
+})
+
+$(".close").click(function(){
+	$(".overlay").css("display", "none");
+})
+
+
+
+
+});
